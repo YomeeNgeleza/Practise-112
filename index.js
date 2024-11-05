bf48af7b2eca670858f941de38460d9eb71417e3
YomeeNgeleza <yomingeleza@gmail.com>
2024-11-05 11:40:38 +0200

    Adding audio to my html file

---
+++ index.js
@@ -0,0 +1,14 @@
+const audioPlayer = document.getElementById("#audioPlayer");
+
+function previousTrack() {
+  console.log("Playing previous track");
+}
+
+function playAudio() {
+  audioPlayer.play();
+  console.log("Playing audio");
+}
+
+function nextTrack() {
+  console.log("Playing next track");
+}
